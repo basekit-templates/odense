ae710f0612c4846e102bfbdb8d94fee088374545
rhysjparry <hello@rhys.jp>
2015-05-18 10:48:49 +0100

    Removed LESS error.

--- js/main.js
+++ js/main.js
@@ -5,12 +5,12 @@ $(function() {
 
 // Toggles class open to display/hide the whole navigation
 $( ".navigation-toggle" ).click(function() {
-		$( ".navigation-body, .bk-extendednavigation" ).toggleClass( "open" );
+	$( ".navigation-body, .bk-extendednavigation" ).toggleClass( "open" );
 });
 
 // Toggles class open to display/hide folders
 $( ".navigation-item.folder" ).each(function() {
 	$( this ).click(function() {
-			$( this ).toggleClass( "open" );
+		$( this ).toggleClass( "open" );
 	});
 });
