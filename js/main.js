340e38f2efc5a7d5fe1759d9ebe964e449087434
rhysjparry <hello@rhys.jp>
2015-06-22 14:14:47 +0100

    Testing header changes.

--- js/main.js
+++ js/main.js
@@ -4,15 +4,16 @@ $( ".navigation-toggle" ).click(function() {
 	$( ".navigation-body, .bk-extendednavigation, .template-nav" ).toggleClass( "open" );
 });
 
-// Toggles class open to display/hide folders
-$( ".navigation-item.folder" ).each(function() {
+ // Toggles class open to display/hide folders
+$( ".navigation-item.folder .item-name--parent" ).each(function() {
 	$( this ).click(function() {
-		$( this ).toggleClass( "open" );
+		$( this ).parent( ".navigation-item.folder" ).toggleClass( "open" );
 	});
 });
 
+// Closes shopping basket modal when clicking on the outer overlay
 $('.basket-body').click(function(e) {
     if (e.target == this) { 
-    	$(".widget").removeClass("show-content");
+    	$("#page-zones__template-widgets__ecombasket-shopbasket").removeClass("show-content");
     }
 });
