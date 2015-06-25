62d33b538f58392d0faa0e44e4a304c25684eac9
rhysjparry <hello@rhys.jp>
2015-06-25 13:41:50 +0100

    Reworked JS to try and fix editor bugs

--- js/main.js
+++ js/main.js
@@ -1,23 +1,10 @@
 
-// Toggles class open to display/hide the whole navigation
-$( ".navigation-toggle" ).click(function() {
-	$( ".navigation-body, .template-nav, .basket-toggle, .navigation-toggle, .ecombasket" ).toggleClass( "open" );
-});
-
  // Toggles class open to display/hide folders
 $( ".navigation-item.folder .item-name--parent" ).each(function() {
 	$( this ).click(function() {
 		$( this ).parent( ".navigation-item.folder" ).toggleClass( "open" );
 	});
 });
-
-/*
-$('.basket-body').click(function(e) {
-    if (e.target == this) { 
-    	$("#page-zones__template-widgets__ecombasket-shopbasket").removeClass("show-content");
-    }
-});*/
-
 
 $( ".ecombasket__basket-toggle" ).click(function() {
 	$( "body" ).toggleClass( "basket-open" );
@@ -28,4 +15,9 @@ $('.basket-body').click(function(e) {
 	if (e.target == this) {
 	$( "body" ).removeClass( "basket-open" );
 	}
+});
+
+// Toggles class open to display/hide the whole navigation
+$( ".navigation-toggle" ).click(function() {
+	$( "body" ).toggleClass( "nav-open" );
 });
